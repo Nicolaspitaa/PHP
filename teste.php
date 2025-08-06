ba20ed714fe2f32454e8f7b1c7f932f8219bd9af
Nicolas Pita <nicolaspitar@gmail.com>
2025-08-06 11:06:09 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -1,6 +1,40 @@
 <?php
 
-echo "Ola Mundo PHP!";
+    // VARIAVEIS E TIPOS DE DADOS
+    
+
+    $nome = "Nicolas";
+
+    echo"Olá meu nome é $nome <br> <br>" ;
+
+    $sobrenome = "Pita";
+
+
+
+    //CONCATENAÇÃO UNIAO DE STRING E UNIAO DE VARIAVEIS
+    $nomecompleto = $nome . " " . $sobrenome;
+
+    
+    echo"Meu nome é $nomecompleto <br> <br> ";
+
+//inteiro
+
+$idade = 24;
+
+echo"Eu tenho $idade anos <br> <br>";
+
+$somaidade = $idade + 5;
+
+echo"$somaidade <br> <br>";
+
+
+// floats nmeros quebrados
+
+$altura = 1.75;
+echo "Minha altura é $altura <br><br>";
+
+
+
 
 
 ?>
